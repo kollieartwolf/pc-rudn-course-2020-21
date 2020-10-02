036eca45304a85fd6829282744df560b85996f83
kollieartwolf <titoffklim@cclc.tech>
2020-10-02 19:43:35 +0300

    Добавлен базовый сервер Django
    Созданы модели постов, изменена стартовая страница курса, добавлена лицензия MIT

---
+++ srv/lectures/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+
+
+class OpenedManager(models.Manager):
+    def get_queryset(self):
+        return super(OpenedManager, 
+                     self).get_queryset().filter(status='opened')
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (('unshown', 'Unshown'),
+                      ('opened', 'Opened'),
+                      )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    short_desc = models.TextField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=10, 
+                              choices=STATUS_CHOICES, 
+                              default='unshown')
+    objects = models.Manager()
+    opened = OpenedManager()
+    
+    
+    def get_absolute_url(self):
+        return reverse('lectures:post_detail', 
+                       args=[self.publish.year, 
+                             self.publish.month, 
+                             self.publish.day, self.slug])
+
+    
+    class Meta:
+        ordering = ('publish',)
+        
+        def __str__(self):
+            return self.title
---
+++ srv/lectures/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.opened.all()
+    return render(request, 
+                  'post/list.html', 
+                  {'posts': posts})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, 
+                             status='opened', 
+                             publish__year=year, 
+                             publish__month=month, 
+                             publish__day=day)
+    return render(request, 
+                  'post/lecture.html', 
+                  {'post': post})
